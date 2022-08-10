fb50dc2a443261771376c3772bb3c27a9cbd91a8
“Translogik” <vano0402@gmail.com>
2022-08-10 17:26:46 +0300

    Add full FirstVersion project

---
+++ js/forma.js
@@ -0,0 +1,7 @@
+function onFormaClick()
+{
+	var shadow=dociment.getElementByID('shadow')
+	var forma1=dociment.getElementByID('forma1')
+	shadow.style.display = "block";
+	forma1.style.display = "block";
+}
